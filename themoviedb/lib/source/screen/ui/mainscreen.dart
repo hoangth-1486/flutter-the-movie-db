21e3b817d5b9992ac6d0e028767c420a0734b078
thang.huy.hoang <FRAMGIA\thang.huy.hoang@framgia0384-pc.framgia.com>
2019-04-08 08:32:27 +0700

    Init project

---
+++ themoviedb/lib/source/screen/ui/mainscreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MainWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("The Movie DB"),
+      ),
+      body: Center(
+        child: Text("hihi"),
+      ),
+    );
+  }
+  
+}
